d5798d37eee98aff27370bd652b2a9a6df5c2c93
Jon Nguyen <jon.mail@hotmail.com>
2020-04-29 01:20:54 -0700

    pickled model and script to api call

--- application/routes.py
+++ application/routes.py
@@ -1,14 +1,18 @@
-from flask import request, render_template, make_response, jsonify
+from flask import request, jsonify
 from flask import current_app as app
+import numpy as np 
+import pandas as pd 
+import json
+import pickle
 
-# import json
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# import pickle
+# pickled vectorizer and classifier
+with open('vectorizer.pkl', 'rb') as file:
+    vec_pickle = pickle.load(file)
 
-# pickled multiple regression model
-# clf = pickle.load(open('PICKLED_CLF.pkl','rb'))
-# vec = pickle.load('PICKLED_VECTORIZER.pkl')
+with open('SGDClassifier.pkl', 'rb') as file1:
+    clf_pickle = pickle.load(file1)
 
+# endpoint for sending the post    
 @app.route('/predict', methods=['POST'])
 def make_predict():
     # read in data
@@ -16,27 +20,20 @@ def make_predict():
     
     # get variables
     title = text_inputs['title']
-    selftext = text_inputs['selftext']
+    selftext = text_inputs['body']
     
     # combine
     text = title + ' ' + selftext
-    text = list(text)
 
-
-    def vectorize(text):
-        # vect should be pickled here
-        test_sparse = vect.transform([test_input])
-        return test_sparse
+    # vect should be pickled here
+    text_vect = vec_pickle.transform([text])
     
     # make prediction USING PICKLED MODEL and convert it to list so that jsonify is happy
-    # Ideally the pickled pipeline should take the input all the way to output list
-    output = clf(test_sparse.todense(), n_neighbors=5)
-    rec_id_list = test_array[1][0]
-    recommendations = data.iloc[rec_id_list]["subreddit"]
+    output = clf_pickle.predict(text_vect)
 
     # send back the top 5 subreddits and their associated probabilities
 
-    return jsonify(top_five = recommendations)
+    return jsonify(output[0])
     
 
 
