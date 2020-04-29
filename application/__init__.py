e1482762de781917feaa17c3e4a07e964e99a5c1
Jon Nguyen <jon.mail@hotmail.com>
2020-04-29 09:51:23 -0700

    take out db

--- application/__init__.py
+++ application/__init__.py
@@ -9,10 +9,10 @@ def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
-    db.init_app(app)
+    # db.init_app(app)
 
-    with app.app_context():
-        from . import routes  # Import routes
-        db.create_all()  # Create database tables for our data models
+    # with app.app_context():
+        # from . import routes  # Import routes
+        # db.create_all()  # Create database tables for our data models
 
-        return app
+    return app
